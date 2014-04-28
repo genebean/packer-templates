eeebc396903151d2c49dd71d32754feea8aad160
GeneBean <me@technicalissues.us>
2014-04-27 22:30:33 -0400

    Added a reboot and a sleep to allow new kernels to become active. This is needed so that when the cleanup runs it will be able to remove the old one.

--- centos-6.5/scripts/base.sh
+++ centos-6.5/scripts/base.sh
@@ -2,4 +2,7 @@ sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
 yum -y install yum-presto
 yum -y install binutils fuse-libs gcc gcc-c++ kernel-devel-`uname -r` make perl yum-utils
+
 yum -y upgrade
+reboot
+sleep 60
