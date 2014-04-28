de5a47ab645c69e77ff1f1f6da9c094f87750b1b
GeneBean <me@technicalissues.us>
2014-04-27 22:05:29 -0400

    Moved yum presto to it's own install line so it is available for subsequent installs. Added a line to upgrade everything. Added a cleanup step to remove old kernels.

--- centos-6.5/scripts/base.sh
+++ centos-6.5/scripts/base.sh
@@ -1,3 +1,5 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install binutils fuse-libs gcc gcc-c++ kernel-devel-`uname -r` make perl yum-presto yum-utils
 
+yum -y install yum-presto
+yum -y install binutils fuse-libs gcc gcc-c++ kernel-devel-`uname -r` make perl yum-utils
+yum -y upgrade
