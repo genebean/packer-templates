9a89db10a397b8206f402f2f6deb07631d4665e7
Gene Liverman <gliverma@westga.edu>
2014-05-20 23:21:39 -0400

    Added ntpdate to the setup to resolve an issue where yum / epel was failing due to the time not being set correctly.

--- centos-6.5/scripts/base.sh
+++ centos-6.5/scripts/base.sh
@@ -1,6 +1,10 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-yum -y install yum-presto
+yum -y install yum-presto ntpdate
+
+# Ensure date is correct so that yum does not fail due to the time being off
+ntpdate -s time.nist.gov
+
 yum -y install binutils fuse-libs gcc gcc-c++ kernel-devel-`uname -r` make perl yum-utils
 
 yum -y upgrade
