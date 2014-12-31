aff07f94a56dafef86f80d029897bdb91e000b54
GeneBean <me@technicalissues.us>
2014-12-30 20:27:09 -0500

    Applied my customizations to CentOS 6.6

--- centos-6.6/scripts/base.sh
+++ centos-6.6/scripts/base.sh
@@ -1,3 +1,12 @@
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-yum -y install gcc make gcc-c++ kernel-devel-`uname -r` perl
 
+yum -y install yum-presto ntpdate
+
+# Ensure date is correct so that yum does not fail due to the time being off
+ntpdate -s time.nist.gov
+
+yum -y install binutils fuse-libs gcc gcc-c++ kernel-devel-`uname -r` make perl yum-utils
+
+yum -y upgrade
+reboot
+sleep 60
--- centos-6.6/scripts/vagrant.sh
+++ centos-6.6/scripts/vagrant.sh
@@ -4,3 +4,5 @@ mkdir -pm 700 /home/vagrant/.ssh
 curl -L https://raw.githubusercontent.com/mitchellh/vagrant/master/keys/vagrant.pub -o /home/vagrant/.ssh/authorized_keys
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant:vagrant /home/vagrant/.ssh
+
+echo 'UseDNS no' >> /etc/sshd_config
