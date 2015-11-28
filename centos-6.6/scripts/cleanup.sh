c7bc3a9c1d17d1ffb1a313122344141bedef73af
Gene Liverman <me@technicalissues.us>
2015-11-27 22:11:59 -0500

    Removed a bunch of old files that I don't use

--- centos-6.6/scripts/cleanup.sh
+++
@@ -1,5 +0,0 @@
-package-cleanup --oldkernels --count 1
-yum -y clean all
-rm -rf VBoxGuestAdditions_*.iso
-rm -rf /tmp/rubygems-*
-
--- centos-6.6/scripts/epel.sh
+++
@@ -1,2 +0,0 @@
-yum -y localinstall http://reflector.westga.edu/repos/Fedora-EPEL/6/x86_64/epel-release-6-8.noarch.rpm
-yum -y install dkms
