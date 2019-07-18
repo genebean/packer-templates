b37cfb67c231f43a87e2d84e1b3060dc4d58b031
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-07-18 11:53:53 -0400

    Removed box version check
    Since auto-uploads are no more this isn't needed

--- centos-7/build.sh
+++ centos-7/build.sh
@@ -6,11 +6,6 @@ cd $DIR
 rm -f boxes/*
 rm -rf output-*
 
-if [ -z "$VAGRANT_BOX_VERSION" ]; then
-  echo "VAGRANT_BOX_VERSION must be set"
-  exit 1
-fi
-
 if [ "$#" -ne 1 ]; then
   echo "usage: $0 [virtualbox|vmware]"
   exit 1
