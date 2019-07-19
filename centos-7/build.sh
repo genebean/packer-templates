09b8d28f2e4aa4d8c9fa7a23972ad4cdc4c2b951
Gene Liverman <gene.liverman+06301990@puppet.com>
2019-07-19 12:43:35 -0400

    Adjust so only what's being built gets cleaned up

--- centos-7/build.sh
+++ centos-7/build.sh
@@ -3,9 +3,6 @@
 DIR="$(cd "$(dirname "$0")" && pwd -P)"
 cd $DIR
 
-rm -f boxes/*
-rm -rf output-*
-
 if [ "$#" -ne 1 ]; then
   echo "usage: $0 [virtualbox|vmware]"
   exit 1
@@ -14,6 +11,8 @@ fi
 case "$1" in
   virtualbox|vmware )
     builder=$1
+    rm -f boxes/*${builder}*
+    rm -rf output-${builder}*
     ;;
   * )
     echo "The only builders that are currently supported are virtualbox and vmware."
